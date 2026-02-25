703b749c6fbad233ca8c55741d5162779ed880a6
Andrew-Israil <andrewkerollos@gmail.com>
2026-02-25 19:42:10 +0100

    Removed build

--- build/tests/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/andrew/cppforcv/igg_image/tests
-# Build directory: /home/andrew/cppforcv/igg_image/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(initial_matrix_test "/home/andrew/cppforcv/igg_image/bin/initial_matrix_test")
-set_tests_properties(initial_matrix_test PROPERTIES  _BACKTRACE_TRIPLES "/home/andrew/cppforcv/igg_image/tests/CMakeLists.txt;20;add_test;/home/andrew/cppforcv/igg_image/tests/CMakeLists.txt;0;")
-subdirs("../gtest")
